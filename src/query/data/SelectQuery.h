f8868d960052bfa87673320022909f079ffc332e
MrXueBao <zhangyihan1@sjtu.edu.cn>
2024-11-24 02:29:10 +0800

    feat(p2m1): select query

---
+++ src/query/data/SelectQuery.h
@@ -0,0 +1,19 @@
+#ifndef SELECTQUERY_H
+#define SELECTQUERY_H
+
+#include "../Query.h"
+#include "../QueryResult.h"
+#include <string>
+
+class SelectQuery : public ComplexQuery {
+  static constexpr const char *qname = "SELECT";
+
+public:
+  using ComplexQuery::ComplexQuery;
+
+  QueryResult::Ptr execute() override;
+
+  std::string toString() override;
+};
+
+#endif // SELECTQUERY_H
