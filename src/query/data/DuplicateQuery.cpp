5cc4fa276db4e688aa0bce35a578e2d100d44af6
MrXueBao <zhangyihan1@sjtu.edu.cn>
2024-11-23 14:21:10 +0800

    feat(p2m1): main body of DuplicateQuery::execute function

--- src/query/data/DuplicateQuery.cpp
+++ src/query/data/DuplicateQuery.cpp
@@ -3,6 +3,27 @@
 
 constexpr const char *DuplicateQuery::qname;
 
+QueryResult::Ptr DuplicateQuery::execute() {
+  using namespace std;
+  Database &db = Database::getInstance();
+  Table::SizeType counter = 0;
+
+  try {
+    return make_unique<SuccessMsgResult>(counter);
+  } catch (const TableNameNotFound &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                       "No such table."s);
+  } catch (const IllFormedQueryCondition &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable, e.what());
+  } catch (const invalid_argument &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                       "Unknown error '?'"_f % e.what());
+  } catch (const exception &e) {
+    return make_unique<ErrorMsgResult>(qname, this->targetTable,
+                                       "Unknown error '?'."_f % e.what());
+  }
+}
+
 std::string DuplicateQuery::toString() {
   return "QUERY = DUPLICATE " + this->targetTable + "\"";
 }
