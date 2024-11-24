f14144905c27680b61982423ab6f17b1fb2fd32d
unknown <2918834117@qq.com>
2024-11-24 15:15:35 +0800

    remove select

--- src/query/management/DeleteQuery.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by lenovo on 2024/11/23.
-//
-
-#include "DeleteQuery.h"
--- src/query/management/DeleteQuery.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by lenovo on 2024/11/23.
-//
-
-#ifndef DELETEQUERY_H
-#define DELETEQUERY_H
-
-#endif //DELETEQUERY_H
