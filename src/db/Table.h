e2a57e801e8237301c9bbe66ffaea24539fe297e
MrXueBao <zhangyihan1@sjtu.edu.cn>
2024-12-01 16:01:51 +0800

    test(p2m2/0): joj

--- src/db/Table.h
+++ src/db/Table.h
@@ -65,21 +65,27 @@ private:
     Datum(const Datum &) = default;
     Datum &operator=(const Datum &) = default;
 
-    explicit Datum(const SizeType &size) {
-      datum = std::vector<ValueType>(size, ValueType());
-    }
+    // explicit Datum(const SizeType &size) {
+    //   datum = std::vector<ValueType>(size, ValueType());
+    // }
+    explicit Datum(const SizeType &size) : datum(size, ValueType()) {}
 
+    // template <class ValueTypeContainer>
+    // explicit Datum(const KeyType &key, const ValueTypeContainer &datum) {
+    //   this->key = key;
+    //   this->datum = datum;
+    // }
     template <class ValueTypeContainer>
-    explicit Datum(const KeyType &key, const ValueTypeContainer &datum) {
-      this->key = key;
-      this->datum = datum;
-    }
+    explicit Datum(const KeyType &key, const ValueTypeContainer &datum)
+        : key(key), datum(datum.begin(), datum.end()) {}
 
-    explicit Datum(const KeyType &key,
-                   std::vector<ValueType> &&datum) noexcept {
-      this->key = key;
-      this->datum = std::move(datum);
-    }
+    // explicit Datum(const KeyType &key,
+    //                std::vector<ValueType> &&datum) noexcept {
+    //   this->key = key;
+    //   this->datum = std::move(datum);
+    // }
+    explicit Datum(const KeyType &key, std::vector<ValueType> &&datum) noexcept
+        : key(key), datum(std::move(datum)) {}
   };
 
   typedef std::vector<Datum>::iterator DataIterator;
