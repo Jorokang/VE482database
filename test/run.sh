95facd5b4c75f8f7dcefc6f31709e7bdc401ed39
MrXueBao <zhangyihan1@sjtu.edu.cn>
2024-12-02 00:50:59 +0800

    chore(p2m3): prepare for release

--- test/run.sh
+++ test/run.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# This script is for testing the program on server.
-# It will auto build the program and run the test cases, including correctness and multithread performance.
-
 echo "No test cases for now. All test cases have been tested properly on server."
 exit 0
