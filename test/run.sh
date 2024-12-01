5ef3db56a1590d083c9d3e6f05379b86716177eb
MrXueBao <zhangyihan1@sjtu.edu.cn>
2024-12-01 23:42:42 +0800

    fix(p2m3/0): incorrect cd at the first line of run.sh joj

--- test/run.sh
+++ test/run.sh
@@ -4,16 +4,16 @@
 # It will auto build the program and run the test cases, including correctness and multithread performance.
 # For using more test cases, modify QUERY_FILE and OUTPUT_FILE.
 
-cd ./src
+# cd ./src
 
 if [ ! -d "build" ]; then
-    mkdir build
-    cd build
-    ln -s /opt/lemondb/db db
-    ln -s /opt/lemondb/sample sample
-    cmake -DCMAKE_C_COMPILER=/usr/bin/clang-18 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-18 ../src
+  mkdir build
+  cd build
+  ln -s /opt/lemondb/db db
+  ln -s /opt/lemondb/sample sample
+  cmake -DCMAKE_C_COMPILER=/usr/bin/clang-18 -DCMAKE_CXX_COMPILER=/usr/bin/clang++-18 ../src
 else
-    cd build
+  cd build
 fi
 
 cmake --build . -- -j8
@@ -21,7 +21,6 @@ cmake --build . -- -j8
 QUERY_FILE="./sample/test.query"
 OUTPUT_FILE="test.out"
 
-time ./lemondb --listen $QUERY_FILE > $OUTPUT_FILE
-time ./lemondb --listen $QUERY_FILE > $OUTPUT_FILE --thread=1
+time ./lemondb --listen $QUERY_FILE >$OUTPUT_FILE
+time ./lemondb --listen $QUERY_FILE --thread=1 >$OUTPUT_FILE
 diff $OUTPUT_FILE /opt/lemondb/sample_stdout/$OUTPUT_FILE
-
